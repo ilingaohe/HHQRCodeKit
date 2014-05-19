05c639179069b93752c12c0d580cf5744d49495a
lingaohe <ilingaohe@gmail.com>
2014-05-19 20:31:42 +0800

    first init

---
+++ HHQRCodeKit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HHQRCodeKit
+//
+//  Created by GaoheLin on 5/19/14.
+//  Copyright (c) 2014 TangoMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HHQRCodeKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HHQRCodeKit
+//
+//  Created by GaoheLin on 5/19/14.
+//  Copyright (c) 2014 TangoMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
